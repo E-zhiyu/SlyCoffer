454757320a68b281ff793eb42e72d2e4b5355d7f
E_zhiyu <E82487318@outlook.com>
2026-09-22 10:33:30 +0800

    修改更新日志

--- app/src/main/java/com/sly/coffer/helpers/AboutHelper.java
+++ app/src/main/java/com/sly/coffer/helpers/AboutHelper.java
@@ -12,6 +12,10 @@ import com.sly.coffer.ui.others.dialogs.MarkdownDialogBuilder;
 public class AboutHelper {
     private static final String UPDATE_LOG_MD = "# v1.12.2\n" +
             "\n" +
+            "### BUG修复\n" +
+            "\n" +
+            "- 修复通知规则输入界面通过点击关闭图标去除所有分组时意外隐藏标签列表的BUG\n" +
+            "\n" +
             "### 优化和修改的内容\n" +
             "\n" +
             "- 优化部分界面的输入框边距，提升曲面屏用户的使用体验\n" +
