f5ac2ca5b6b059389066aadd8e69deb7d8f98f74
E_zhiyu <E82487318@outlook.com>
2026-09-24 15:03:50 +0800

    添加关于通知规则分组的功能说明

--- app/src/main/java/com/sly/coffer/ui/pages/notification/group/NotificationRuleGroupListActivity.java
+++ app/src/main/java/com/sly/coffer/ui/pages/notification/group/NotificationRuleGroupListActivity.java
@@ -24,6 +24,7 @@ import com.sly.coffer.data.save.db.services.NotificationRuleService;
 import com.sly.coffer.databinding.ActivityNotificationRuleGroupListBinding;
 import com.sly.coffer.helpers.ExceptionHelper;
 import com.sly.coffer.helpers.appearence.AppearanceHelper;
+import com.sly.coffer.ui.others.dialogs.MarkdownDialogBuilder;
 
 import java.util.Locale;
 
@@ -73,6 +74,38 @@ public class NotificationRuleGroupListActivity extends AppCompatActivity {
         });
         AppearanceHelper.setMarginToNavigation(binding.addFab, this);
         AppearanceHelper.attachMorphAnimation(binding.addFab);
+
+        //帮助按钮
+        binding.helpBtn.setOnClickListener(view -> {
+            final String EXPLANATION = "### 1. 功能概述\n" +
+                    "有时进行一次交易后会有多个通知在短时间内被发送，若不设置分组会导致短时间内触发多个金额相同的规则。为了尽可能去除重复的规则触发，可以将多个规则添加至同一个分组中，处于同一个分组的规则仅会触发一个。\n" +
+                    "\n" +
+                    "### 2. 规则互斥原理\n" +
+                    "\n" +
+                    "当解析到符合通知规则的通知时，APP的处理流程如下：\n" +
+                    "1. 缓冲一定时间内触发的多个通知规则；\n" +
+                    "2. 依次遍历缓冲区内的通知规则，若规则不属于任何一个分组，直接触发；\n" +
+                    "3. 按照分组排序从上到下的顺序扫描缓冲区中的通知规则；\n" +
+                    "4. 每个分组仅触发找到的优先级最高的规则；\n" +
+                    "5. 每个分组中触发过一个规则后，会将该规则所在分组中所有优先级更低的规则从缓冲区移除，从而达到互斥目的；\n" +
+                    "6. 重复3~5步，直到所有分组都遍历完毕。\n" +
+                    "\n" +
+                    "> 例：第一组中有A、B规则，第二组中有B、C规则。当三个规则都成功匹配通知内容时，第一组中A触发，同时排斥B，由于B被排斥，第二组中C触发，最后成功触发A、C规则。\n" +
+                    "\n" +
+                    "### 3. 分组和规则的优先级\n" +
+                    "\n" +
+                    "- 规则分组具有优先级，排序越靠前的分组优先级越高，优先级高的分组会优先尝试触发其中的规则。\n" +
+                    "\n" +
+                    "> 例：第一组中包含A、B规则，第二组包含B、A规则。当A、B规则都尝试触发时，由于第一组优先级更高，因此A触发，B被排斥。\n" +
+                    "\n" +
+                    "- 分组内的规则具有优先级，排序越靠前的优先级越高，APP会按照优先级由高到低依次尝试触发通知规则，当触发一个规则后排斥所有低优先级的规则。\n" +
+                    "\n" +
+                    "> 例：第一组包含A、B、C规则，第二组包含B、D、E规则，当B触发时，C、D、E规则都会被排斥。\n" +
+                    "\n";
+            new MarkdownDialogBuilder(this, "功能介绍", EXPLANATION)
+                    .setNegativeButton("关闭", null)
+                    .show();
+        });
 
         //列表
         RuleGroupCardListAdapter adapter = new RuleGroupCardListAdapter(
